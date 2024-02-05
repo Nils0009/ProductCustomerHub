d6c7e87083780acaa23be97f3394bd5372ec5bb0
Nils <Nils0009@hotmail.com>
2024-02-05 17:53:27 +0100

    Two services created CustomerManagement and OrderPaymentManagement service

--- Infrastructure/Services/CustomerManagementService.cs
+++ Infrastructure/Services/CustomerManagementService.cs
@@ -1,6 +1,19 @@
-﻿namespace Infrastructure.Services;
+﻿using Infrastructure.Repositories;
+
+namespace Infrastructure.Services;
 
 public class CustomerManagementService
 {
+    private readonly AddressRepository _addressRepository;
+    private readonly CustomerAddressRepository _customerAddressRepository;
+    private readonly CustomerRepository _customerRepository;
+    private readonly RoleRepository _roleRepository;
 
+    public CustomerManagementService(AddressRepository addressRepository, CustomerAddressRepository customerAddressRepository, CustomerRepository customerRepository, RoleRepository roleRepository)
+    {
+        _addressRepository = addressRepository;
+        _customerAddressRepository = customerAddressRepository;
+        _customerRepository = customerRepository;
+        _roleRepository = roleRepository;
+    }
 }
---
+++ Infrastructure/Services/OrderPaymentManagementService.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Repositories;
+
+namespace Infrastructure.Services;
+
+public class OrderPaymentManagementService
+{
+    private readonly OrderRepository _orderRepository;
+    private readonly PaymentMethodRepository _paymentMethodRepository;
+
+    public OrderPaymentManagementService(OrderRepository orderRepository, PaymentMethodRepository paymentMethodRepository)
+    {
+        _orderRepository = orderRepository;
+        _paymentMethodRepository = paymentMethodRepository;
+    }
+}
