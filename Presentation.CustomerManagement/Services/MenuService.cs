f6e8d24491a6e3c84e26ecbc9d2b13265d9bdebf
Nils <Nils0009@hotmail.com>
2024-02-06 11:49:19 +0100

    MenuService for order and customer management is under development

--- Presentation.CustomerManagement/Services/MenuService.cs
+++ Presentation.CustomerManagement/Services/MenuService.cs
@@ -13,6 +13,75 @@ public class MenuService(CustomerManagementService customerManagementService, Or
 		try
 		{
 			Console.WriteLine("##MENU##");
+			Console.WriteLine();
+			Console.WriteLine("[1] Customer Management");
+			Console.WriteLine("[2] Order Management");
+            int.TryParse(Console.ReadLine(), out int userOption);
+
+			switch (userOption)
+			{
+				case 1:
+					Console.WriteLine("Customer menu");
+					Console.WriteLine();
+					Console.WriteLine("[1] Add Customer");
+                    Console.WriteLine("[2] Get Customer");
+                    Console.WriteLine("[3] Get All Customers");
+                    Console.WriteLine("[4] Update Customer");
+                    Console.WriteLine("[5] Delete Customer");
+                    Console.WriteLine("[6] Exit");
+                    int.TryParse (Console.ReadLine(), out int userCustomerOption);
+
+                    switch (userCustomerOption)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+				case 2:
+                    Console.WriteLine("Order menu");
+                    Console.WriteLine();
+                    Console.WriteLine("[1] Add Order");
+                    Console.WriteLine("[2] Get Order");
+                    Console.WriteLine("[3] Get All Orders");
+                    Console.WriteLine("[4] Update Order");
+                    Console.WriteLine("[5] Delete Order");
+                    Console.WriteLine("[6] Exit");
+
+                    int.TryParse(Console.ReadLine(), out int userOrderOption);
+                    switch (userOrderOption)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+				default:
+					break;
+			}
 		}
 		catch (Exception ex)
 		{
